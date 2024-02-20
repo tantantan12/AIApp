f7783b2b80ac936d0ba3c406481538106c175e33
Jane Xue Tan <janetanxue@gmail.com>
2024-02-20 17:02:06 -0600

    Update fetchAI.js

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -9,7 +9,7 @@ const openai = new OpenAIApi(configuration)
 const handler = async (event) => {
     try {
         const response = await openai.createCompletion({
-            model: 'gpt-3.5-turbo-instruct',
+            model: 'gpt-3.5-turbo',
             prompt: [
                     {"role": "system","content": "You are a marketing specialist."},
                     {"role": "user","content": event.body}] ,
