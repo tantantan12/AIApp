705e5b1563cdd5492ae4119e7941838be8423068
Jane Xue Tan <janetanxue@gmail.com>
2024-02-21 11:54:53 -0600

    Update fetchAI.js

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -8,15 +8,13 @@ const handler = async (event) => {
     try {
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
-            messages: [
-    {role: "system",content: "you are a marketing specialist."},
-    {role: "user", content:  event.body    }] ,
+            messages: [{role: "system",content: "you are a marketing specialist."},{role: "user", content:event.body}] ,
         presence_penalty: 0,
         frequency_penalty: 0.3,
         max_tokens: 100,
         temperature: 0,
-        });
-        console.log(completion.choices[0]);
+        })
+   //     console.log(completion.choices[0]);
         return {
             statusCode: 200,
             body:response.choices[0]  //response.data.choices[0].message.content.trim() //JSON.stringify({reply: response.data})
