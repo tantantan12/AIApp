747883fb90bee98e7702f303a1705a59797d26ad
Jane Xue Tan <janetanxue@gmail.com>
2024-02-22 09:58:10 -0600

    Update fetchAI.js

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,23 +1,34 @@
-import { OpenAI } from 'openai'
+import { Configuration, OpenAIApi } from 'openai'
 
-const openai = new OpenAI({
+const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 })
 
+const openai = new OpenAIApi(configuration)
+
 const handler = async (event) => {
+    
     try {
-        const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
-            messages: [{role: "system",content: "you are a marketing specialist."},{role: "user", content:event.body}] ,
+        const response = await openai.createCompletion({
+            model: 'gpt-3.5-turbo-instruct',
+            prompt: event.body,
         presence_penalty: 0,
         frequency_penalty: 0.3,
         max_tokens: 100,
         temperature: 0,
         })
-   //     console.log(completion.choices[0]);
         return {
             statusCode: 200,
-            body:response.choices[0]  //response.data.choices[0].message.content.trim() //JSON.stringify({reply: response.data})
+            body: JSON.stringify({
+                reply: response.data                
+/*
+Challenge:
+    1. Add a key value pair. The key should be 'reply' 
+       and the value should be response.data.
+    2. Paste the code into fetchAI.js in VS Code and push it 
+       to GitHub to redeploy and see what gets logged out. 
+*/
+            })
         }
     } catch (error) {
         return { statusCode: 500, body: error.toString() }
