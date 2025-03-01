304bef4296fa93969a0ec3e9b5b3d945305c5a5c
Jane Xue Tan <janetanxue@gmail.com>
2025-03-01 00:08:07 -0600

    update

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,10 +1,11 @@
 const { Configuration, OpenAIApi } = require('openai');
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
+const OpenAI = require("openai");
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
 
 const handler = async (event) => {
     
