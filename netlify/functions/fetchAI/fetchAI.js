9529d84f7675b8dfedc46c6c7c6afe1fa7c09d81
Jane Xue Tan <janetanxue@gmail.com>
2025-03-16 16:53:07 -0500

    Update fetchAI.js

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -11,23 +11,19 @@ const handler = traceable(async (event) => {
     
     try { 
         const requestBody = JSON.parse(event.body);
-        const { productName, productDesc, targetMarket } = requestBody;
+        const { characterName1,characterName2, sceneDesc, conflict } = requestBody;
 
         if (!productName || !productDesc || !targetMarket) {
             return { statusCode: 400, body: JSON.stringify({ error: "Missing required fields" }) };
         }
 
-        const input = `Use a product name, a product description and a target market to create advertising copy for a product.
-            ###
-            product name: EcoPure Hydration Bottle
-            product description: A sustainable, vacuum-insulated water bottle that keeps drinks cold for 48 hours and hot for 24 hours. 
-            product target market: environmentally conscious consumers
-            advertising copy: "Stay refreshed and make a difference with the EcoPure Hydration Bottle – the last water bottle you'll ever need. Embrace the power of sustainability with our innovative design, crafted for the eco-warrior in all of us. Whether you're climbing mountains or navigating the urban jungle, keep your drinks ice-cold or steaming hot, all day long. Join the EcoPure movement and quench your thirst for change. #DrinkSustainably #EcoPureAdventure
-            ###
-            product name: ${productName}
-            product description: ${productDesc}
-            product traget market: ${targetMarket}
-            advertising copy: 
+        const input = `Use a positive character, a negative character, a scene, and a conflict to come up with a short story.
+           
+            positive character: ${characterName1}
+            negative character: ${characterName2}
+            scene description: ${sceneDesc}
+            conflict: ${conflict}
+            story: 
             `;
 
         const response = await openai.completions.create({
@@ -35,7 +31,7 @@ const handler = traceable(async (event) => {
             prompt: input,
             presence_penalty: 0,
             frequency_penalty: 0.3,
-            max_tokens: 100,
+            max_tokens: 500,
             temperature: 0,
         })
         return {
